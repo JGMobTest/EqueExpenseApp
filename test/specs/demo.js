ea7145a431ba0c93a192b36fa384f8ed2bc0aa88
jacqueline.george <jacqueline.george@Eque2.com>
2025-10-28 15:05:41 +0000

    webview testing

--- test/specs/demo.js
+++ test/specs/demo.js
@@ -1,9 +1,38 @@
+
 describe('Expense App', ()=>{
 
 it ('ios Testing ', async () =>{
 
-    
-    console.log('App launched successfully')
-  })
+  await $("~Webview").click();
 
-})
+  //1. get your multiple contexts with this code 
+  await driver.waitUntil(async () =>{
+      const contexts = await driver.getContexts();
+      return contexts.length >1;
+    }, {timeout: 30000,
+        timeoutMsg: 'Sorry, its taking so long. Waiting for another context'});
+
+   //2. Switch to webdrive context 
+
+   const contexts = await driver.getContexts();
+   await driver.switchContext(contexts[1])
+
+   //3. Confirm webview context 
+   const subtitleTxt = await $('.hero__subtitle');
+   await expect(subtitleTxt).toHaveText('Next-gen browser and mobile automation test framework for Node.js');
+
+
+
+
+    
+
+
+    //console.log('App launched successfully')
+  });//end of test case 
+
+
+
+
+
+
+})//end of describe block 
